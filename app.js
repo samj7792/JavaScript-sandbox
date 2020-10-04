423373cfff7abc96c8da34c8fb84deb04b24a8be
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-04 10:50:53 -0400

    Template Literals

--- app.js
+++ app.js
@@ -194,3 +194,42 @@
   // val = str.includes('Tom');
   // console.log(val);
 }
+
+// Template Literals
+{
+  const name = 'John';
+  const age = 30;
+  const job = 'Web Developer';
+  const city = 'Miami';
+
+  // Without template strings (es5)
+  // html =
+  //   '<ul><li>Name: ' +
+  //   name +
+  //   '</li><li>Age: ' +
+  //   age +
+  //   '</li><li>Job: ' +
+  //   job +
+  //   '</li><li>City: ' +
+  //   city +
+  //   '</li></ul>';
+
+  function hello() {
+    return 'Hello';
+  }
+
+  // With template strings (es6)
+  html = `
+    <ul>
+      <li>Name: ${name}</li>
+      <li>Age: ${age}</li>
+      <li>Job: ${job}</li>
+      <li>City: ${city}</li>
+      <li>${2 + 2}</li>
+      <li>${hello()}</li>
+      <li>${age > 21 ? 'Over 21' : 'Under 21'}</li>
+    </ul>
+  `;
+
+  document.body.innerHTML = html;
+}
