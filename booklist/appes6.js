8caba6a2330f6871e4065679bf2b6d0bbb535f3f
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-09 11:27:20 -0400

    added local storage

--- booklist/appes6.js
+++ booklist/appes6.js
@@ -55,7 +55,51 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      const ui = new UI();
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    let books = Store.getBooks();
+
+    books = books.filter((book) => book.isbn !== isbn);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 // Event Listeners
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+
 // event listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   e.preventDefault();
@@ -78,6 +122,10 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   } else {
     // Add book to list
     ui.addBookToList(book);
+
+    // Add to LS
+    Store.addBook(book);
+
     // Show success
     ui.showAlert('Book Added', 'success');
 
@@ -95,6 +143,9 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
   // Delete book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show Message
   ui.showAlert('Book Removed!', 'success');
