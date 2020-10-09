9a3f0675ebe52a804fd291db2a0873b9ed388e5f
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-09 08:37:10 -0400

    Prototypal Inheritance

--- app.js
+++ app.js
@@ -766,4 +766,44 @@
   }
 
   // Prototypal Inheritance
+  {
+    // Person Constructor
+    function Person(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+
+    // Greeting
+    Person.prototype.greeting = function () {
+      return `Hello there ${this.firstName} ${this.lastName}`;
+    };
+
+    const person1 = new Person('John', 'Doe');
+    console.log(person1.greeting());
+
+    // Customer constructor
+    function Customer(firstName, lastName, phone, membership) {
+      Person.call(this, firstName, lastName); // allows us to call another function
+      this.phone = phone;
+      this.membership = membership;
+    }
+
+    // Inherit the Person prototype methods
+    Customer.prototype = Object.create(Person.prototype);
+
+    // Make Customer.prototype return Customer()
+    Customer.prototype.constructor = Customer;
+
+    // Create customer
+    const customer1 = new Customer('Tom', 'Smith', '222-222-2222', 'Standard');
+
+    console.log(customer1);
+
+    // Customer greeting
+    Customer.prototype.greeting = function () {
+      return `Hello there ${this.firstName} ${this.lastName} welcome to our company.`;
+    };
+
+    console.log(customer1.greeting()); // not a function because the customer is not inheriting the greeting until we inherit above
+  }
 }
