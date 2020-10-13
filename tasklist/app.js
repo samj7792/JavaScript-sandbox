3b428bf3cdc6e1766ad8401b48d54c515f43e500
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-13 11:49:55 -0400

    Task List: Delete and Filter tasks

--- tasklist/app.js
+++ tasklist/app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear all tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -32,6 +38,7 @@ function addTask(e) {
   const link = document.createElement('a');
   // Add class
   link.className = 'delete-item secondary-content';
+  link.href = '#modal1';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove" />';
   // Append the link to li
@@ -42,4 +49,37 @@ function addTask(e) {
 
   // Clear input
   taskInput.value = '';
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure you would like to remove this task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear all tasks
+function clearTasks(e) {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach((task) => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
