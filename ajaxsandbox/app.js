be4d2b064d084f739adffe04d8a1281815dbb5cc
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-19 17:05:52 -0400

    Working with ajax and JSON

--- ajaxsandbox/app.js
+++ ajaxsandbox/app.js
@@ -57,3 +57,74 @@
     // 404: 'Not Found'
   }
 }
+
+// Working with Ajax and JSON
+{
+  document.getElementById('button1').addEventListener('click', loadCustomer);
+
+  document.getElementById('button2').addEventListener('click', loadCustomers);
+
+  // load customer
+  function loadCustomer(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customer.json', true);
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        console.log(this.responseText);
+
+        const customer = JSON.parse(this.responseText);
+
+        console.log(customer);
+
+        const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+
+        document.getElementById('customer').innerHTML = output;
+      }
+    };
+
+    xhr.send();
+  }
+
+  // load customers
+  function loadCustomers(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        console.log(this.responseText);
+
+        const customers = JSON.parse(this.responseText);
+
+        console.log(customers);
+
+        let output = '';
+
+        customers.forEach((customer) => {
+          output += `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+        });
+
+        document.getElementById('customers').innerHTML = output;
+      }
+    };
+
+    xhr.send();
+  }
+}
