97888c3ab099cea7738ee7caa44c452e4cd87144
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-23 13:32:47 -0400

    Chuck Norris Joke mini project

--- ajaxsandbox/app.js
+++ ajaxsandbox/app.js
@@ -128,3 +128,36 @@
     xhr.send();
   }
 }
+
+// Chuck Norris Jokes
+{
+  document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+  function getJokes(e) {
+    e.preventDefault();
+
+    const number = document.getElementById('number').value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+
+        let output = '';
+
+        if (response.type === 'success') {
+          response.value.forEach((item) => (output += `<li>${item.joke}</li>`));
+        } else {
+          output += '<li>Something went wrong</li>';
+        }
+
+        document.querySelector('.jokes').innerHTML = output;
+      }
+    };
+
+    xhr.send();
+  }
+}
