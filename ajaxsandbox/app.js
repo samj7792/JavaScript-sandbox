88661a6f0bf456a7b2f66844c63fad06462086e4
Sam Jackson <samuelsjackson7@gmail.com>
2020-10-27 11:12:20 -0400

    Callbacks

--- ajaxsandbox/app.js
+++ ajaxsandbox/app.js
@@ -161,3 +161,34 @@
     xhr.send();
   }
 }
+
+// Callback Function
+{
+  const posts = [
+    {
+      title: 'Post One',
+      body: 'This is post one',
+    },
+    {
+      title: 'Post Two',
+      body: 'This is post two',
+    },
+  ];
+
+  function createPost(post, callback) {
+    setTimeout(function () {
+      posts.push(post);
+      callback();
+    }, 2000);
+  }
+
+  function getPosts() {
+    setTimeout(function () {
+      let output = '';
+      posts.forEach((post) => (output += `<li>${post.title}</li>`));
+      document.getElementById('posts').innerHTML = output;
+    }, 1000);
+  }
+
+  createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
+}
